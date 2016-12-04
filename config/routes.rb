24538f089d59e79fa90fb3e1b11fd34146cbdf10
tyler zhang <263720544@qq.com>
2016-12-04 16:28:33 +0800

    20161104

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
   # get 'article/index'
   #
   # get 'book/index'
@@ -8,4 +10,6 @@ Rails.application.routes.draw do
   resources :article #对应article——controller
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root :to=>'book#index'
+  # 以下是定义根网址的，as部分是告诉rails创建store_path的变量
+  root :to=>'store#index',:as=>'store'
 end
