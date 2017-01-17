327b227675c4067cf88c1506ea9f85c47601929f
tyler zhang <263720544@qq.com>
2017-01-18 01:07:52 +0800

    国际化部分

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -56,7 +56,7 @@ class CartsController < ApplicationController
   def destroy
     @cart.destroy
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to carts_url, notice: '购物车删除成功.' }
       format.json { head :no_content }
     end
   end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -44,7 +44,7 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
+        format.html { redirect_to @line_item, notice: '购物车明细更新成功.' }
         format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit }
@@ -60,7 +60,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
     # LineItem.find(params[:id]).destroy;
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to line_items_url, notice: '购物车明细删除成功.' }
       format.json { head :no_content }
     end
   end
